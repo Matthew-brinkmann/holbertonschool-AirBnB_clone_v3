4e2631e93aba2260318be8ec1c9d1d6d2c13a0a2
declannoble <declanjnoble@hotmail.com>
2022-09-21 14:12:53 +1000

    build: commiting for refactor

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,48 +1,49 @@
 #!/usr/bin/python3
-"""
-flask application module for retrieval of
-State Objects
-"""
+"""module to start flask app"""
 from api.v1.views import app_views
-from flask import abort, jsonify, request
+from flask import abort, jsonify, make_response, request
 from models import storage
+from models.city import City
 from models.state import State
 
 
-@app_views.route('/states',
-                 methods=['GET', 'POST'],
+@app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'],
                  strict_slashes=False)
-def get_all_states():
-    """Retrieves the list of all State objects"""
-    print("in correct route")
+def get_cityList_createCity(state_id):
+    """retrieves a list of city objects or creates new city"""
     if request.method == 'GET':
-        returnedValue, code = State.api_get_all(
-                    storage.all("State").values()
+        returnedValue, code = City.api_get_all(
+            storage.all("State", state_id).values
         )
     if request.method == 'POST':
-        returnedValue, code = State.api_post(
-                    ["name"],
-                    request.get_json(silent=True))
+        returnedValue, code = City.api_post(
+            ['name'],
+            request.get_json(silent=True)
+        )
+    if code == 404:
+        abort(404)
     return (jsonify(returnedValue), code)
 
 
-@app_views.route('/states/<string:state_id>',
-                 methods=['GET', 'DELETE', 'PUT'],
+@app_views.route('/cities/<city_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
-def state_by_id(state_id):
-    """handles State object: state_id"""
+def get_post_delete_city(city_id):
+    """retrieves, deletes or updates a city object"""
     if request.method == 'GET':
-        returnedValue, code = State.api_get_single(
-                        storage.get("State", state_id))
+        returnedValue, code = City.api_get_single(
+            storage.get('City', city_id)
+        )
     if request.method == 'DELETE':
-        returnedValue, code = State.api_delete(
-                    storage.get("State", state_id))
+        returnedValue, code = City.api_delete(
+            storage.get("City", city_id)
+        )
     if request.method == 'PUT':
-        returnedValue, code = State.api_put(
-                    ['id', 'created_at', 'updated_at'],
-                    request.get_json(silent=True),
-                    storage.get("State", state_id))
+        returnedValue, code = City.api_put(
+            ['id', 'state_id', 'created_at', 'updated_at'],
+            request.get_json(silent=True),
+            storage.get('City', city_id)
+        )
     if code == 404:
         abort(404)
     storage.save()
-    return (jsonify(returnedValue), code)
+    return(jsonify(returnedValue), code)
